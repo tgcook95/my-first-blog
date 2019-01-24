a9d89a9e87d20e548a958cfa7c52c75ad67ea69f
Taylor <Taylor@example.com>
2019-01-23 20:29:11 -0600

    Fixed venv and Modified templates to display posts from database.

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cookt\google drive\school\2019 spring\web app dev\djangogirls\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
